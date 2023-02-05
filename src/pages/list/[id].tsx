51a8aaec4fe865f9e7f0920a4b6a4197e1ddaa59
abnerpersio <binhopersio@gmail.com>
2023-02-05 17:56:43 -0300

    fix: styles file location

--- src/pages/list/[id].tsx
+++ src/pages/list/[id].tsx
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/router';
-
-import { Container } from './styles';
+import { Container } from '~/styles/list';
 
 export default function List() {
   const { query } = useRouter();
