5ec757a01a12095b31e2a7a68243366ba9687e2d
abnerpersio <binhopersio@gmail.com>
2023-03-29 23:55:49 -0300

    style: confirm page

--- src/pages/invite/confirm/index.tsx
+++ src/pages/invite/confirm/index.tsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Button } from '~/components/button';
+import { GoBackIcon } from '~/components/icons/go-back';
 import { Input } from '~/components/input';
+import { Modal } from '~/components/modal';
 import { Page } from '~/components/page';
 
-import { Content } from './styles';
+import { Content, InfoModalContent } from './styles';
 
 export function InviteConfirm() {
   const { id } = useParams();
   const [guests, setGuests] = useState<number>(0);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
 
   console.log(id);
 
@@ -18,10 +21,31 @@ export function InviteConfirm() {
     setGuests(value);
   };
 
+  const handleOpenModal = () => setModalOpen(true);
+  const handleCloseModal = () => setModalOpen(false);
+
   return (
     <Page isLoading={false}>
+      <Modal modalId="invite-confirm-info" isOpen={modalOpen} onClose={handleCloseModal}>
+        <InfoModalContent>
+          <p>Cada convidado (junto de seus acompanhantes) tem seu próprio link.</p>
+
+          <p>
+            Se houve um engano com o nome escrito nesse formulário, não o preencha. Entre em contato
+            com quem te enviou para receber o seu link personalizado
+          </p>
+        </InfoModalContent>
+      </Modal>
+
       <Content>
         <h2>Olá, Abner!</h2>
+
+        <div className="actions">
+          <GoBackIcon />
+          <Link to={`/invite/${id}`}>Voltar</Link>
+          <small>|</small>
+          <span onClick={handleOpenModal}>Não sou Abner</span>
+        </div>
 
         <div className="input-group">
           <p>Número de acompanhantes</p>
--- src/pages/invite/confirm/styles.tsx
+++ src/pages/invite/confirm/styles.tsx
@@ -10,6 +10,37 @@ export const Content = styled.main`
     width: 100%;
   }
 
+  h2 {
+    text-align: left;
+    font-size: 1.75em;
+  }
+
+  div.actions {
+    width: 100%;
+    margin-bottom: 16px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 4px;
+    font-size: 0.85em;
+    opacity: 0.8;
+
+    svg {
+      fill: ${({ theme }) => theme.colors.black};
+      width: 0.75em;
+      height: 0.75em;
+    }
+
+    a {
+      color: ${({ theme }) => theme.colors.black};
+      text-decoration: none;
+    }
+
+    small {
+      margin: 0px 8px;
+    }
+  }
+
   div.input-group {
     width: 100%;
     justify-content: space-between;
@@ -20,4 +51,14 @@ export const Content = styled.main`
   small {
     font-size: 0.7em;
   }
+`;
+
+export const InfoModalContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  opacity: 0.8;
+  color: ${({ theme }) => theme.colors.black};
+  font-size: 1em;
 `;
